1641cc3f868489528d15e0c2f2110f9f14c0579f
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-08 17:56:50 +0530

    Check

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ function add() {
   var id = document.getElementById("ID").value;
   var name = document.getElementById("name").value;
   var salary = document.getElementById("salary").value;
+  var upsal=document.getElementById("up").value;
   
 
   mytable =
-    "<table><tr><th>ID</th><th>Nmae</th><th>Salary</th><th>Action</th></tr>";
+    "<table><tr><th>ID</th><th>Nmae</th><th>Salary</th><th>Updated Salary</th><th>Action</th></tr>";
 
   var product = {
     ID: id,
     Name: name,
     Salary: salary,
+    Updated_sal:upsal,
   };
   cart.push(product);
 
@@ -40,19 +42,29 @@ function display(){
 }
 
 class Emp {
-    function (){
+    employee (){
         var Emp_id=document.getElementById("ID").value;
         var Emp_name=document.getElementById("name").value;
         var Emp_salary=document.getElementById("salary").value;
-    
-    var Emp_ob = {
-        Emp_ID: Emp_id,
-        Emp_Name: Emp_name,
-        Emp_Salary: Emp_salary,
+        
+        var Emp_ob = {
+        Emp_ID: 'Emp_id',
+        Emp_Name: 'Emp_name',
+        Emp_Salary: 'Emp_salary',
       };
+       var st=JSON.stringify(Emp_ob);
       cart.push(Emp_ob);
       add();
       display();
     }
-  
+    
 }
+
+
+
+    
+
+
+
+
+
