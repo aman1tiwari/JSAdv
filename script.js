e0c5ac27d1382b8a864c57953867ea349c7fee85
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-14 17:21:13 +0530

    Using classes and objects

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var cart = [];
 function add() {
-  var id = document.getElementById("ID").value;
+  var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
   var salary = document.getElementById("salary").value;
-  var upsal=document.getElementById("up").value;
-  
+  var upsal = document.getElementById("up").value;
 
   mytable =
     "<table><tr><th>ID</th><th>Nmae</th><th>Salary</th><th>Updated Salary</th><th>Action</th></tr>";
@@ -13,15 +12,15 @@ function add() {
     ID: id,
     Name: name,
     Salary: salary,
-    Updated_sal:upsal,
+    Updated_sal: upsal,
   };
   cart.push(product);
 
   display();
 }
 
-function display(){
-    mytable =
+function display() {
+  mytable =
     "<table><tr><th>ID</th><th>Name</th><th>Salary</th><th>Action</th></tr>";
 
   cart.forEach((element) => {
@@ -33,38 +32,32 @@ function display(){
       "</td><td>" +
       element.Salary +
       "</td><td>" +
-      "<button>Delete</button>"
-    "</td></tr>";
+      "<button>Delete</button>";
+    ("</td></tr>");
   });
   mytable += "</table>";
   // console.log(mytable);
   document.getElementById("table1").innerHTML = mytable;
 }
 
-class Emp {
-    employee (){
-        var Emp_id=document.getElementById("ID").value;
-        var Emp_name=document.getElementById("name").value;
-        var Emp_salary=document.getElementById("salary").value;
-        
-        var Emp_ob = {
-        Emp_ID: 'Emp_id',
-        Emp_Name: 'Emp_name',
-        Emp_Salary: 'Emp_salary',
-      };
-       var st=JSON.stringify(Emp_ob);
-      cart.push(Emp_ob);
-      add();
-      display();
+class employee {
+  empfun() {
+    var id = document.getElementById("id").value;
+    var name = document.getElementById("name").value;
+    var sal = document.getElementById("salary").value;
+
+    var data = {
+      Id: `${id}`,
+      Name: `${name}`,
+      Sal: `${sal}`,
+    };
+    cart.push(data);
+    console.log(person);
+
+    var e1 = new employee();
+    function addperson() {
+      e1.empfun();
+      e1.display();
     }
-    
+  }
 }
-
-
-
-    
-
-
-
-
-
