f24665040bc0e581c8782780b6ce3a315e305e51
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-14 18:19:06 +0530

    showing updated salary of the employee

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add() {
   var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
   var salary = document.getElementById("salary").value;
-  var upsal = document.getElementById("up").value;
+  var upsal = (document.getElementById("salary").value)*(1.10)
 
   mytable =
     "<table><tr><th>ID</th><th>Nmae</th><th>Salary</th><th>Updated Salary</th><th>Action</th></tr>";
@@ -21,7 +21,7 @@ function add() {
 
 function display() {
   mytable =
-    "<table><tr><th>ID</th><th>Name</th><th>Salary</th><th>Action</th></tr>";
+    "<table><tr><th>ID</th><th>Name</th><th>Salary</th><th>Updated Salary</th><th>Action</th></tr>";
 
   cart.forEach((element) => {
     mytable +=
@@ -31,6 +31,8 @@ function display() {
       element.Name +
       "</td><td>" +
       element.Salary +
+      "</td><td>" +
+      element.Updated_sal +
       "</td><td>" +
       "<button>Delete</button>";
     ("</td></tr>");
@@ -60,4 +62,23 @@ class employee {
       e1.display();
     }
   }
+}
+
+try {
+  col=""
+  cart.forEach((element) => {
+  var val =element.Updated_sal*1.10
+  col += `  <tr>
+  <td>${element.ID}</td>
+  <td>${element.Name}</td>
+  <td>${element.Salary}</td>
+  <td>${val}</td>
+  </tr>`;
+  });
+  document.getElementById("table").innerHTML = table + col;
+  document.getElementById("id").value = "";
+  document.getElementById("Name").value = "";
+  document.getElementById("salary").value = "";
+} catch (error) {
+  console.log(error);
 }
